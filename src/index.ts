15078e218205fb7e25fe740ebcd81f9292f2aa3d
zack zheng <zack_zhengxiyun@163.com>
2025-07-18 11:59:40 +0800

    add --name

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
-import figlet from 'figlet';
-import gradient from 'gradient-string';
-import boxen from 'boxen';
+/* eslint-disable no-console */
 
-const title = figlet.textSync('zack-xy', {
+import process from 'node:process'
+import boxen from 'boxen'
+import chalk from 'chalk'
+import figlet from 'figlet'
+import gradient from 'gradient-string'
+
+// 获取命令行参数
+
+const args = process.argv.slice(2)
+const nameIndex = args.indexOf('--name')
+const name = nameIndex !== -1 && args[nameIndex + 1] ? args[nameIndex + 1] : 'zack-xy'
+
+const title = figlet.textSync(name, {
   horizontalLayout: 'full',
-});
+})
 
-console.log(gradient.pastel.multiline(title));
+console.log(gradient.pastel.multiline(title))
 console.log(
-  boxen(chalk.cyan('Welcome to Zack Logs! 🎉'), {
+  boxen(chalk.cyan(' 🎉 Hi！I\'m Zack Zheng(<zack_zhengxiyun@163.com>) 🎉 '), {
     padding: 1,
     margin: 1,
     borderStyle: 'double',
     borderColor: 'green',
-  })
-);
+  }),
+)
