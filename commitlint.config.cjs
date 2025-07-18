8d989a3cdf8d1c9a54c04755b00b4b4e3897a296
zack zheng <zack_zhengxiyun@163.com>
2025-07-18 16:48:56 +0800

    wip: update

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   // 继承的规则
   extends: ['@commitlint/config-conventional'],
   // 定义规则
-  roles: {
+  rules: {
     // type的类型定义: 表示git提交的type必须在以下类型范围之内
     'type-enum': [
       // 当前验证错误级别
